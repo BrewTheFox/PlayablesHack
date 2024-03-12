d73583a6132d9ee5df4a48ef83b6fb51f8f362b1
BrewTheFox <brew@example.com>
2024-03-12 18:13:39 -0500

    Website K

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
-import { useState } from 'react'
 import tomblogo from './assets/TotM_Logo.webp'
+import slicelogo from './assets/SliceItAllLogo-transformed.webp'
+import statelogo from "./assets/240312_18h04m38s_screenshot-transformed.png"
+import climberlogo from "./assets/240312_18h10m59s_screenshot-transformed.png"
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <>
-    <div className='card'>
+    <Navbar></Navbar>
+    <div className='container'>
+    <div>
       <img className="logo" src={tomblogo}/>
+    </div>
+    <div>
+      <img className="logo" src={slicelogo}/>
+    </div>
+    <div>
+      <img className="logo" src={statelogo}/>
+    </div>
+    <div>
+      <img className="logo" src={climberlogo}/>
+    </div>
     </div>
     </>
   )
