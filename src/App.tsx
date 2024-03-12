d725e0b7e4aa0388d35c6842f7789ce57ea6b37a
BrewTheFox <PaoloMartinelliMagdaniel@proton.me>
2024-03-12 16:43:15 -0500

    Commit Inicial, NO GUI

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import tomblogo from './assets/TotM_Logo.webp'
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <div className='card'>
+      <img className="logo" src={tomblogo}/>
+    </div>
+    </>
+  )
+}
+
+export default App
