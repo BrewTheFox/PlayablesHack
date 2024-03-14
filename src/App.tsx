e44dad4e1f3a678e3e25c23d744d3b2690e5621b
BrewTheFox <PaoloMartinelliMagdaniel@proton.me>
2024-03-14 17:45:15 -0500

    update names

---
+++ src/App.tsx
@@ -0,0 +1,41 @@
+import tomblogo from './assets/TotM_Logo.webp'
+import slicelogo from './assets/SliceItAllLogo-transformed.webp'
+import statelogo from "./assets/240312_18h04m38s_screenshot-transformed.png"
+import climberlogo from "./assets/240312_18h10m59s_screenshot-transformed.png"
+import './App.css'
+import Navbar from './components/Navbar'
+import Typewriter from 'typewriter-effect';
+
+
+function App() {
+  return (
+    <>
+    <Navbar></Navbar>
+    <div className='typewriter'>
+    <Typewriter
+  options={{
+    strings: ['HACK', 'IT', 'IF', 'YOU', 'CAN'],
+    autoStart: true,
+    loop: true,
+  }} 
+  ></Typewriter>
+    </div>
+    <div className='container'>
+    <div>
+      <img className="logo" src={tomblogo}/>
+    </div>
+    <div>
+      <img className="logo" src={slicelogo}/>
+    </div>
+    <div>
+      <img className="logo" src={statelogo}/>
+    </div>
+    <div>
+      <img className="logo" src={climberlogo}/>
+    </div>
+    </div>
+    </>
+  )
+}
+
+export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './mainsite.tsx'
+import App from './App.tsx'
 import './index.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Totm from './tombofthemask.tsx'
