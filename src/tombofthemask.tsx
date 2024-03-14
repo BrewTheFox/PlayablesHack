a8f3236638758592cd3fc31081764e708accf40c
BrewTheFox <PaoloMartinelliMagdaniel@proton.me>
2024-03-14 14:41:02 -0500

    update css

--- src/tombofthemask.tsx
+++ src/tombofthemask.tsx
@@ -2,8 +2,10 @@ import tomblogo from './assets/TotM_Logo.webp'
 import './App.css'
 import Navbar from './components/Navbar'
 import Typewriter from 'typewriter-effect';
-
 import { TextareaAutosize } from '@mui/material';
+
+
+
 function Totm() {
   return (
     <>
@@ -11,7 +13,16 @@ function Totm() {
     <div className='gameiconcontainer'>
     <img className="gameicon" src={tomblogo}/>
     </div>
-    <TextareaAutosize></TextareaAutosize>
+    <div className='supracontainer'>
+    <div className="codecontainer">
+      <TextareaAutosize
+        className="textarea"
+        aria-label="code"
+        placeholder="Escribe el codigo curl..."
+        minRows={20} // Número mínimo de filas
+      />
+    </div>
+    </div>
     </>
   )
 }
