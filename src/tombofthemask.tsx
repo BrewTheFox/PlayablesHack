2d78c0b46abe9e10bb23c0e0a1f8efdd08232f86
BrewTheFox <brew@example.com>
2024-03-16 14:43:01 -0500

    change url to vercel

--- src/tombofthemask.tsx
+++ src/tombofthemask.tsx
@@ -56,7 +56,7 @@ function Totm() {
     let config = {
       method: 'post',
       maxBodyLength: Infinity,
-      url: 'http://127.0.0.1:8000/do-request/',
+      url: 'https://playablesback.vercel.app/do-request/',
       headers: { 
         'Content-Type': 'application/json'
       },
